e8a2eb250e1bc2935f3951a733c9196a5cb9da52
Allyson <77851830+Allyson-F@users.noreply.github.com>
2022-04-21 22:29:32 -0400

    Update pigs_disease.js

--- pages/pigs_disease.js
+++ pages/pigs_disease.js
@@ -1,7 +1,7 @@
 function pigs_disease() {
   return (<div>
             <div>
-            You can view the Machado Lab&apos;s website&nbsp
+            You can view the Machado Lab&apos;s website&nbsp;
               <a href="https://machado-lab.github.io/">here
               </a>
             </div>
