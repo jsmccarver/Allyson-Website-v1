ebc77003909d1b86aa3662a54e09b3e59aac5eea
Allyson <77851830+Allyson-F@users.noreply.github.com>
2022-04-21 22:19:44 -0400

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 function about() {
   return (
     <div>
-      <p>Hello! I&apos;m Allyson! I have a BSc in Geology from NCSU and am finishing up a graduate certificate in geographic information systems. I really like making cute maps!</p>
+      <p>Hello! I&apos;m Allyson! I have a BSc in Geology from NCSU and am finishing up a graduate certificate in geographic information systems. I really like making cute maps! I have three cats!</p>
     </div>
   );
 }
